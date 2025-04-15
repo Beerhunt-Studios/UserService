ab6cd050d5b5db2c2518e24a33160a77a7d8913a
Kimon Meier <kimon.meier@gmail.com>
2025-04-15 20:24:14 +0200

    Added an AutoMapper Test for testing all configurations

---
+++ test/UserService.Application.Tests/MappingTest.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using UserService.Application.Interfaces.Repositories;
+using Xunit;
+
+namespace UserService.Application.Tests;
+
+public class MappingTest
+{
+    [Fact]
+    public static void TestMapping()
+    {
+        var configuration = new MapperConfiguration(cfg => cfg.AddMaps(typeof(AssemblyDefinition).Assembly));
+
+        configuration.AssertConfigurationIsValid();
+    }
+
+
+}
