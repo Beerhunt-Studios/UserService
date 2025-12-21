5b18c4675ee64e9365e981aad0972f9861394346
Kimon Meier <kimon.meier@gmail.com>
2025-12-21 21:49:33 +0100

    Added environment variable support and base builder configuration in Program.cs

--- src/UserService.Api/Program.cs
+++ src/UserService.Api/Program.cs
@@ -1,11 +1,15 @@
 ﻿using BaseChord.Api;
 using BaseChord.Api.Logging;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using UserService.Application;
 using UserService.Infrastructure;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddEnvironmentVariables();
+builder.WebHost.ConfigureBaseBuilder(builder.Configuration);
 
 builder.AddCustomizedLogging();
 
@@ -14,7 +18,6 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
 app.ConfigureBaseApp();
 
 app.Run();
